1e6a773d0fad86d725041c087a78bc22663447af
Miguel <gandalaxxx@gmail.com>
2020-08-10 16:29:38 +0200

    updating stripe integration and updating our favicon and title

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51GuFQwEGbOOiC4bEg3Yo9uRT9IaqGxakAo6FPk1p0bJgcc8QtnMRuJN8evYqHRvMH3uv861S8B8vcfgJRr7mFVwe00OrtVxqpN';
 
   const onToken = token => {
     console.log(token);
